2fc9dad29b547fff80199bfb1e9d5b8136b62b15
Alton8 <133280761+Alton8@users.noreply.github.com>
2025-05-19 13:45:08 -0700

    Update BigSpender.java

--- BigSpender.java
+++ BigSpender.java
@@ -10,7 +10,7 @@ public class BigSpender extends User {
         quotes = new ArrayList<>();
         quotes.add("You treat your bank account like it wronged you in a past life.");
         quotes.add("Good news: You’re stimulating the economy. Bad news: It’s just your economy.");
-        quotes.add("You spent $" + price + " on... honestly, we don’t even know. Neither do you.");
+        quotes.add("You spent " + this.getStringPrice() + " on... honestly, we don’t even know. Neither do you.");
         quotes.add("We ran the numbers and... yikes.");
         quotes.add("Were you feeding yourself or opening a small bistro?");
         quotes.add("You didn’t split the bill — you became the bill.");
