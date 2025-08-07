f8fdfd3cb7b8eafcfe9836989736e01af4120bb5
Alton8 <133280761+Alton8@users.noreply.github.com>
2025-08-06 23:22:00 -0700

    Update BigSpender.java
    Added some new methods!

--- BigSpender.java
+++ BigSpender.java
@@ -7,15 +7,7 @@ public class BigSpender extends User {
         super(name, price);
 
         quotes = new ArrayList<>();
-        quotes.add("You treat your bank account like it wronged you in a past life.");
-        quotes.add("Good news: You’re stimulating the economy. Bad news: It’s just your economy.");
-        quotes.add("You spent " + getStringPrice() + " on... honestly, we don’t even know.");
-        quotes.add("We ran the numbers and... yikes.");
-        quotes.add("Were you feeding yourself or opening a small bistro?");
-        quotes.add("You didn’t split the bill — you became the bill.");
 
-        int randomIndex = (int)(Math.random() * quotes.size());
-        setFunnyMessage(quotes.get(randomIndex)); // Store once in constructor
     }
     public BigSpender(String name, double price, String funnyMessage) {
         super(name, price);
@@ -24,9 +16,28 @@ public class BigSpender extends User {
 
     @Override
     public String funnyMessage() {
+        quotes.add("You treat your bank account like it wronged you in a past life.");
+        quotes.add("Good news: You’re stimulating the economy. Bad news: It’s just your economy.");
+        quotes.add("We ran the numbers and... yikes.");
+        quotes.add("Were you feeding yourself or opening a small bistro?");
+        quotes.add("You didn’t split the bill — you became the bill.");
+
+        int randomIndex = (int)(Math.random() * quotes.size());
+        setFunnyMessage(quotes.get(randomIndex)); 
         return super.funnyMessage();
     }
-
+    @Override
+    public ArrayList<String> getQuotes() {
+        return quotes;
+    }
+    @Override
+    public void addFunnyMessage(String message) {
+        quotes.add(message);
+    }
+    @Override 
+    public String getType() {
+        return "BigSpender";
+    }
     @Override
     public String toString() {
         return getName() + " owes a whole " + getStringPrice() + " dollars.";
