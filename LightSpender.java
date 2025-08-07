c8be4be40b39fd6c0f0619ef144bfb6c258cf471
Alton8 <133280761+Alton8@users.noreply.github.com>
2025-08-06 23:21:46 -0700

    Update LightSpender.java

--- LightSpender.java
+++ LightSpender.java
@@ -7,14 +7,7 @@ public class LightSpender extends User {
         super(name, price);
 
         quotes = new ArrayList<>();
-        quotes.add("Thrifty legend. You spent less than a large coffee.");
-        quotes.add("You treated yourself... very lightly.");
-        quotes.add("Your wallet didn’t even notice this purchase.");
-        quotes.add("You spent " + this.getStringPrice() + " — you’re practically a financial guru.");
-        quotes.add("You’re the kind of person coupon sites dream about.");
 
-        int randomIndex = (int)(Math.random() * quotes.size());
-        setFunnyMessage(quotes.get(randomIndex)); // Store once
     }
     public LightSpender(String name, double price, String funnyMessage) {
         super(name, price);
@@ -22,9 +15,27 @@ public class LightSpender extends User {
     }
     @Override
     public String funnyMessage() {
+        quotes.add("Thrifty legend. You spent less than a large coffee.");
+        quotes.add("You treated yourself... very lightly.");
+        quotes.add("Your wallet didn’t even notice this purchase.");
+        quotes.add("You’re the kind of person coupon sites dream about.");
+
+        int randomIndex = (int)(Math.random() * quotes.size());
+        setFunnyMessage(quotes.get(randomIndex)); // Store once
         return super.funnyMessage();
     }
-
+    @Override
+    public ArrayList<String> getQuotes() {
+        return quotes;
+    }
+    @Override
+    public void addFunnyMessage(String message) {
+        quotes.add(message);
+    }
+    @Override 
+    public String getType() {
+        return "LightSpender";
+    }
     @Override
     public String toString() {
         return getName() + " only spent " + getStringPrice() + ". Nice!";
